a329283c566f1f8b330b09bc81eb49bee7f89ad5
Nikita Aplin <aplinxy9plin@icloud.com>
2018-03-27 15:29:58 +0700

    Commit

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { WebView } from 'react-native';
+import { Button } from 'react-native';
+
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}}>
+          <TextInput
+            style={{height: 40}}
+            placeholder="Type here to translate!"
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Text></Text>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '20px',
+  },
+});
